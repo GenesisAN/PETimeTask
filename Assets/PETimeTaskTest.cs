f7a2556ffe24a9df57ac49013bfb7d335ca2f289
AN <501946815@qq.com>
2021-07-24 15:52:48 +0800

    Initialize repository

---
+++ Assets/PETimeTaskTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FinTOKMAK.PETimeTask;
+
+public class PETimeTaskTest : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeSystem.Instance.AddTimeTask((id,dt,it,c) => {
+
+            this.gameObject.SetActive(false);
+        
+        }, 2, PETimeUnit.Second, 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Package/Runtime/PETimeTask.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FinTOKMAK.PETimeTask
+{
+
+    public class PETimeTask
+    {
+        /// <summary>
+        /// 任务ID
+        /// </summary>
+        public int tid;
+        /// <summary>
+        /// 下次执行时间(毫秒)
+        /// </summary>
+        public float destTime;
+        /// <summary>
+        /// 回调方法（任务ID，下次执行时间(毫秒)，延迟间隔(毫秒)，执行次数(为0时则无限执行)
+        /// </summary>
+        public Action<int,float,float,int> callback;
+        /// <summary>
+        /// 每次执行的延迟间隔（毫秒）
+        /// </summary>
+        public float delay;
+        public int count;
+        /// <summary>
+        /// PE定时回调任务
+        /// </summary>
+        /// <param name="tid">任务id</param>
+        /// <param name="callback">回调方法（任务ID，下次执行时间(毫秒)，延迟间隔(毫秒)，执行次数(为0时则无限执行)</param>
+        /// <param name="destTime">下次执行时间</param>
+        /// <param name="delay">延迟间隔</param>
+        /// <param name="count">次数</param>
+        public PETimeTask(int tid, Action<int, float, float, int> callback, float destTime,float delay,int count)
+        {
+            this.tid = tid;
+            this.callback = callback;
+            this.destTime = destTime;
+            this.delay = delay;
+            this.count = count;
+        }
+    }
+    public enum PETimeUnit
+    {
+        /// <summary>
+        /// 动画时间单位，60个动画单位=1000ms
+        /// </summary>
+        Millisecond,
+        Second,
+        Minute,
+        Hour,
+        Day
+
+    }
+}
